e697b824aab8dd03161792bb535c873367f1b391
itamar klein <itamarkln@gmail.com>
2024-07-02 16:32:26 +0300

    [FEATURE] create video editor toolbar

--- src/app/features/video-editor/video-editor.component.ts
+++ src/app/features/video-editor/video-editor.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-video-editor',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './video-editor.component.html',
   styleUrl: './video-editor.component.scss'
 })
