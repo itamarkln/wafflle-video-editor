8a29e4aae4186f1d32892e84ab4232604aabdbca
itamar klein <itamarkln@gmail.com>
2024-07-02 15:21:06 +0300

    [IMPROVE] update application's routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from '@layout/page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'video-editor',
+    loadComponent: () => import('@features/video-editor/video-editor.component').then((m) => m.VideoEditorComponent)
+  },
+  { path: '', redirectTo: 'video-editor', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
