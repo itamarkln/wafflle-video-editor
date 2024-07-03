3e948bcd3c97c93be594ed4e170d16b6ca8b99aa
itamar klein <itamarkln@gmail.com>
2024-07-03 11:01:00 +0300

    [FEATURE] create video-editor page that includes previous created components

--- src/app/pages/video-editor/video-editor.page.ts
+++ src/app/pages/video-editor/video-editor.page.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { ScenePreviewComponent } from '@app/features/scenes/components/scene-preview/scene-preview.component';
+import { ScenesComponent } from '@app/features/scenes/scenes.component';
+import { TimelineComponent } from '@app/features/timeline/timeline.component';
 
 @Component({
   selector: 'app-video-editor',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [
+    MatToolbarModule,
+    ScenesComponent,
+    ScenePreviewComponent,
+    TimelineComponent
+  ],
   templateUrl: './video-editor.page.html',
   styleUrl: './video-editor.page.scss'
 })
