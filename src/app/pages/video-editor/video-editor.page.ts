b3566fd57ad5722d8385bbe07aa1b9bd640b6f69
itamar klein <itamarkln@gmail.com>
2024-07-03 20:24:35 +0300

    [IMPROVE] scene & scene-preview layout and styling

--- src/app/pages/video-editor/video-editor.page.ts
+++ src/app/pages/video-editor/video-editor.page.ts
@@ -3,11 +3,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { ScenePreviewComponent } from '@app/features/scenes/components/scene-preview/scene-preview.component';
 import { ScenesComponent } from '@app/features/scenes/scenes.component';
 import { TimelineComponent } from '@app/features/timeline/timeline.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-video-editor',
   standalone: true,
   imports: [
+    MatButtonModule,
+    MatIconModule,
     MatToolbarModule,
     ScenesComponent,
     ScenePreviewComponent,
