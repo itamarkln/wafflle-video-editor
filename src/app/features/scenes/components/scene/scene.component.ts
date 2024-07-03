a0c452c5a4f1b1086a352bad7326d4a6004471a9
itamar klein <itamarkln@gmail.com>
2024-07-03 23:37:39 +0300

    [FEATURE] draggable scene

--- src/app/features/scenes/components/scene/scene.component.ts
+++ src/app/features/scenes/components/scene/scene.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { CdkDrag, CdkDropList } from '@angular/cdk/drag-drop';
+import { Component, InputSignal, input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -7,10 +8,10 @@ import { IScene } from './interfaces/scene.interface';
 @Component({
   selector: 'app-scene',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatCardModule],
+  imports: [MatButtonModule, MatIconModule, MatCardModule, CdkDropList, CdkDrag],
   templateUrl: './scene.component.html',
   styleUrl: './scene.component.scss'
 })
 export class SceneComponent {
-  @Input() scene!: IScene;
+  scene: InputSignal<IScene> = input.required<IScene>();
 }
