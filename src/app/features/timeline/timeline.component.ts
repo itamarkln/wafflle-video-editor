25b02bf3eb9d632c873b051174b1d92395e02521
itamar klein <itamarkln@gmail.com>
2024-07-04 00:34:20 +0300

    [FEATURE] timeline is a drop zone

--- src/app/features/timeline/timeline.component.ts
+++ src/app/features/timeline/timeline.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { CdkDragDrop, DragDropModule } from '@angular/cdk/drag-drop';
+import { Component, InputSignal, Signal, computed, input } from '@angular/core';
+import { IScene } from '../scenes/components/scene/interfaces/scene.interface';
 
 @Component({
   selector: 'app-timeline',
   standalone: true,
-  imports: [],
+  imports: [DragDropModule],
   templateUrl: './timeline.component.html',
   styleUrl: './timeline.component.scss'
 })
 export class TimelineComponent {
+  scenes: InputSignal<IScene[]> = input<IScene[]>([]);
+  timelineScenes: Signal<IScene[]>;
+  ticks: Signal<number[]>;
 
+  constructor() {
+    this.timelineScenes = computed(() => this.scenes());
+    this.ticks = computed(() => {
+      const totalDurationInS = this.timelineScenes().reduce((acc: number, scene: IScene) => acc + scene.duration, 0);
+      return Array(totalDurationInS);
+    });
+  }
+
+  dropped(event: CdkDragDrop<IScene[]>) {
+    const droppedScene = event.previousContainer.data.find((_, index) => index === event.previousIndex);
+    droppedScene && this.timelineScenes().push(droppedScene);
+
+    // const currTimelineScenes = event.container.data;
+    console.log(droppedScene, this.timelineScenes());
+  }
 }
