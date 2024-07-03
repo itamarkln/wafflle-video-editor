67299a0f4b50f951599bbeb7f7feb4f83c1e680b
itamar klein <itamarkln@gmail.com>
2024-07-03 19:45:48 +0300

    [FEATURE] create scenes list component

--- src/app/features/scenes/scenes.component.ts
+++ src/app/features/scenes/scenes.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
-import { SceneComponent } from './components/scene/scene.component';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { ScenePreviewComponent } from './components/scene-preview/scene-preview.component';
+import { IScene } from './components/scene/interfaces/scene.interface';
+import { SceneComponent } from './components/scene/scene.component';
+import { ScenesService } from './services/scenes.service';
 
 @Component({
   selector: 'app-scenes',
@@ -9,6 +12,32 @@ import { ScenePreviewComponent } from './components/scene-preview/scene-preview.
   templateUrl: './scenes.component.html',
   styleUrl: './scenes.component.scss'
 })
-export class ScenesComponent {
+export class ScenesComponent implements OnInit, OnDestroy {
+  private _subscriptions: Subscription[];
+  public scenes: IScene[];
 
+  constructor(private scenesService: ScenesService) {
+    this._subscriptions = [];
+    this.scenes = [];
+  }
+
+  ngOnInit(): void {
+    this.handleSubscriptions();
+  }
+
+  private handleSubscriptions(): void {
+    this.subscribeScenes();
+  }
+
+  private subscribeScenes(): void {
+    this._subscriptions.push(
+      this.scenesService.getScenes().subscribe((scenes: IScene[]) => {
+        this.scenes = scenes;
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this._subscriptions.forEach((sub: Subscription) => sub.unsubscribe());
+  }
 }
--- src/app/features/scenes/services/scenes.service.ts
+++ src/app/features/scenes/services/scenes.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { IScene } from '../components/scene/interfaces/scene.interface';
+import SCENES from '../data/scenes.data.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScenesService {
+  private _scenes: IScene[];
 
-  constructor() { }
+  constructor() {
+    this._scenes = SCENES;
+  }
+
+  public getScenes(): Observable<IScene[]> {
+    return of(this._scenes);
+  }
 }
