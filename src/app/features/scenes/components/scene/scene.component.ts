6afecf3cc986306762d8db191255dded18bfc3e3
itamar klein <itamarkln@gmail.com>
2024-07-03 19:46:03 +0300

    [FEATURE] create scene component

--- src/app/features/scenes/components/scene/scene.component.ts
+++ src/app/features/scenes/components/scene/scene.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { IScene } from './interfaces/scene.interface';
 
 @Component({
   selector: 'app-scene',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule, MatCardModule],
   templateUrl: './scene.component.html',
   styleUrl: './scene.component.scss'
 })
 export class SceneComponent {
-
+  @Input() scene!: IScene;
 }
