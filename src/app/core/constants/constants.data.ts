7554a6ce8051e15307b549536b903cc8b9543ff1
itamar klein <itamarkln@gmail.com>
2024-07-05 18:11:04 +0300

    [FIX] app layout styling

--- src/app/core/constants/constants.data.ts
+++ src/app/core/constants/constants.data.ts
@@ -1,3 +1,3 @@
 // Sidebar
-export const SIDENAV_OPENED_WIDTH = '200px';
-export const SIDENAV_CLOSED_WIDTH = '88px';
+export const SIDENAV_OPENED_WIDTH = '12rem';
+export const SIDENAV_CLOSED_WIDTH = '4rem';
